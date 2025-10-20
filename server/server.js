c67a9c5875656499daefb982a132eb3598baca3d
sinster23 <upayandutta204@gmail.com>
2025-10-20 17:21:36 +0530

    updated home

--- server/server.js
+++ server/server.js
@@ -39,4 +39,4 @@ app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-// export default app;
+export default app;
